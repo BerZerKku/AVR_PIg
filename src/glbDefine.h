a70e410e7060201d53b06b57dff6f8bf50218817
bear <bear@prosoftsystems.ru>
2021-08-30 11:38:04 +0500

    Изменить отображение версии ПИ на 1v39
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- src/glbDefine.h
+++ src/glbDefine.h
@@ -32,7 +32,7 @@
 #define PASSWORD_USER 0
 
 /// версия текущей прошивки
-#define VERS 0x0140
+#define VERS 0x0139
 
 /// максимально кол-во команд на прием (должно быть кратно 8)
 #define MAX_NUM_COM_PRM 32
